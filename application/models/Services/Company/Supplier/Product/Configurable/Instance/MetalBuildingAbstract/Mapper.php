77e15dd6699c4a4cc9ffba272c20eafcf2b84aa3
Jessie Green <jessie.winslows@gmail.com>
2012-10-29 15:42:05 -0500

    test2

--- application/models/Services/Company/Supplier/Product/Configurable/Instance/MetalBuildingAbstract/Mapper.php
+++ application/models/Services/Company/Supplier/Product/Configurable/Instance/MetalBuildingAbstract/Mapper.php
@@ -1,6 +1,6 @@
 <?php
 namespace Services\Company\Supplier\Product\Configurable\Instance\MetalBuildingAbstract;
-//test
+
 class Mapper extends \Services\Company\Supplier\Product\Configurable\Instance\Mapper\MapperAbstract
 {    
     /**
