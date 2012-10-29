198840a247aff0d4790ca18f27da8f7ee3d9cf62
Jessie Green <jessie.winslows@gmail.com>
2012-10-29 16:30:08 -0500

    Revert "jessie Commit"
    This reverts commit 2602fb246f460d1c261450b0fec4875959ecc506.

--- application/models/Services/Company/Supplier/Product/Configurable/Instance/MetalBuildingAbstract/Mapper.php
+++ application/models/Services/Company/Supplier/Product/Configurable/Instance/MetalBuildingAbstract/Mapper.php
@@ -3,7 +3,6 @@ namespace Services\Company\Supplier\Product\Configurable\Instance\MetalBuildingA
 //Jessie
 class Mapper extends \Services\Company\Supplier\Product\Configurable\Instance\Mapper\MapperAbstract
 {
-    //Master
     /**
      * @var Mapper\Data 
      */
